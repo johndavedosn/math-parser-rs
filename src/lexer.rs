5de62a8c1e12354c03e4be3ae8110f5a1d15c0fd
Ali moumen <moumenisbest@gmail.com>
2025-06-05 20:39:17 +0200

    Simplified the lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,10 +1,9 @@
 use logos::Logos;
-use logos::Lexer;
 #[macro_export]
 macro_rules! string {
     ($str:literal) => {{
         $str.to_string()
-    }};
+    }}
 }
 #[derive(Logos, Debug, PartialEq)]
 #[logos(skip r"[ \t\n\f]+")]
@@ -13,36 +12,27 @@ pub enum Tokens {
     Number(i64),
     #[regex(r"[A-Za-z]+",  |lex| lex.slice().to_string())]
     Variable(String),
-    #[regex(r"[+\-*/^=]", which_operation)]
-    Operation(Option<Operations>),
-    #[regex(r"[\(\)]", |lex| lex.slice().to_string())]
-    Paranthesis(String)
-}
-#[derive(Debug, PartialEq)]
-pub enum Operations {
+    #[token("+")]
     Addition,
+    #[token("-")]
     Subtraction,
+    #[token("*")]
     Multiplication,
+    #[token("/")]
     Division,
+    #[token("^")]
     Exponantiation,
-    Equals
+    #[token("=")]
+    Equals,
+    #[token("(")]
+    OpenParan,
+    #[token(")")]
+    CloseParan
 }
-fn which_operation<'s>(lex: &mut Lexer<'s, Tokens>) -> Option<Operations>{
-    match lex.slice() {
-        "+" => Some(Operations::Addition),
-        "-" => Some(Operations::Subtraction),
-        "*" => Some(Operations::Multiplication),
-        "/" => Some(Operations::Division),
-        "^" => Some(Operations::Exponantiation),
-        "=" => Some(Operations::Equals),
-        _ => None
-    }
-}
+
 pub mod tests {
     #[allow(unused_imports)]
     use logos::Logos;
-    #[allow(unused_imports)]
-    use crate::lexer::Operations;
 
     #[cfg(test)]
     use super::Tokens;
@@ -50,9 +40,9 @@ pub mod tests {
     pub fn test_lexer_1() {
         let mut lex = Tokens::lexer("1 + 1 = 3x");
         assert_eq!(lex.next(),  Some(Ok(Tokens::Number(1))));
-        assert_eq!(lex.next(), Some(Ok(Tokens::Operation(Some(Operations::Addition)))));
+        assert_eq!(lex.next(), Some(Ok(Tokens::Addition)));
         assert_eq!(lex.next(), Some(Ok(Tokens::Number(1))));
-        assert_eq!(lex.next(), Some(Ok(Tokens::Operation(Some(Operations::Equals)))));
+        assert_eq!(lex.next(), Some(Ok(Tokens::Equals)));
         assert_eq!(lex.next(), Some(Ok(Tokens::Number(3))));
         assert_eq!(lex.next(), Some(Ok(Tokens::Variable(string!("x")))));
     }
@@ -60,17 +50,17 @@ pub mod tests {
     pub fn test_lexer_2() {
         let mut lex = Tokens::lexer("f(x) = 5x^2 + 3 - 4");
         assert_eq!(lex.next(), Some(Ok(Tokens::Variable(string!("f")))));
-        assert_eq!(lex.next(), Some(Ok(Tokens::Paranthesis(string!("(")))));
+        assert_eq!(lex.next(), Some(Ok(Tokens::OpenParan)));
         assert_eq!(lex.next(), Some(Ok(Tokens::Variable(string!("x")))));
-        assert_eq!(lex.next(), Some(Ok(Tokens::Paranthesis(string!(")")))));
-        assert_eq!(lex.next(), Some(Ok(Tokens::Operation(Some(Operations::Equals)))));
+        assert_eq!(lex.next(), Some(Ok(Tokens::CloseParan)));
+        assert_eq!(lex.next(), Some(Ok(Tokens::Equals)));
         assert_eq!(lex.next(), Some(Ok(Tokens::Number(5))));
         assert_eq!(lex.next(), Some(Ok(Tokens::Variable(string!("x")))));
-        assert_eq!(lex.next(), Some(Ok(Tokens::Operation(Some(Operations::Exponantiation)))));
+        assert_eq!(lex.next(), Some(Ok(Tokens::Exponantiation)));
         assert_eq!(lex.next(), Some(Ok(Tokens::Number(2))));
-        assert_eq!(lex.next(), Some(Ok(Tokens::Operation(Some(Operations::Addition)))));
+        assert_eq!(lex.next(), Some(Ok(Tokens::Addition)));
         assert_eq!(lex.next(), Some(Ok(Tokens::Number(3))));
-        assert_eq!(lex.next(), Some(Ok(Tokens::Operation(Some(Operations::Subtraction)))));
+        assert_eq!(lex.next(), Some(Ok(Tokens::Subtraction)));
         assert_eq!(lex.next(), Some(Ok(Tokens::Number(4))));
 
     }
